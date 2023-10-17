8e38ade168a914d168a479ef10d0a0145f7545bf
Alejandro Lembke Barrientos <aleleba@hotmail.com>
2023-10-17 18:56:22 +0000

    PR-988743: Adding support to Prefix url and updating packages.

--- src/frontend/components/App.tsx
+++ src/frontend/components/App.tsx
@@ -3,9 +3,10 @@ import PrincipalRoutes from './PrincipalRoutes';
 import { config } from '../../../config';
 
 const App = () => {
+	const { PREFIX_URL } = config;
 	if(config.ENV === 'development') {
 		useEffect(() => {
-			const ws = new WebSocket(`wss://${config.HOST}/ws`);
+			const ws = new WebSocket(`wss://${config.HOST}${PREFIX_URL}/ws`);
 			ws.onmessage = (event) => {
 				if (event.data === 'reload') {
 					window.location.reload();
